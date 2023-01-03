4f5417dfad4afd4047275ae3f92fe3e4f2c0f8ab
Andrey Makarov <makarov21a@yandex.ru>
2023-01-03 14:04:59 +0300

    тернарный оперератор

--- day2.py
+++ day2.py
@@ -1,25 +1,8 @@
-# x = -4
-# if x<0:
-#     x = abs(x)
-#
-# print(x)
-# a = float(input("a = "))
-# b = float(input("b = "))
-# if a<b:
-#     a,b = b,a
-# print(f"a = {a}\nb = {b}")
-# x = int(input("x = "))
-# if x >=-4 and x<=10:
-#     print("x в диапозоне от -4 до 10")
-# print(f"x = {x}")
-x = int(input("Введите x = "))
-if x<0:
-    print("x - отрицательное число")
-elif x==0:
-    print("x = 0")
-else:
-    print("x - положительное число")
-
+a =1299
+b = 4777
+c = 67657
+res = (a if a>c else c) if a>b else (b if b>c else c)
+print(res)
 
 
 
