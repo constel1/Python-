92e7af1184009adef19804ce905f4b16dbccfdcf
Andrey Makarov <makarov21a@yandex.ru>
2023-01-03 13:45:37 +0300

    условия if else elif

--- day2.py
+++ day2.py
@@ -1,32 +1,24 @@
-line = [1,7,6,11,3]
-img = [[1,7,6,11,3], [1,7,6,11,3], [1,7,6,11,3], [1,7,6,11,3], [1,7,6,11,3]]
-print(img)
-img = [line[:], line[:], line[:], line[:], line[:]]
-print(img)
-print(img[3])
-print(img[2][1])
-img[1] = [0,0,0,0,0]
-print(img)
-img[1] = [0]*5
-print(img)
-img[1][2:] = [1] *5
-print(img)
-img[2][:] = [4]*6
-print(img)
-img.append("hdfjhdcfj")
-print(img)
-del img[0]
-print(img)
-print(img[4])
-a = [[[True, False],[1,2,3]], ["матрица", "вектор"]]
-print(a[0])#true...
-print(a[0][0][1])
-
-
-
-
-
-
+# x = -4
+# if x<0:
+#     x = abs(x)
+#
+# print(x)
+# a = float(input("a = "))
+# b = float(input("b = "))
+# if a<b:
+#     a,b = b,a
+# print(f"a = {a}\nb = {b}")
+# x = int(input("x = "))
+# if x >=-4 and x<=10:
+#     print("x в диапозоне от -4 до 10")
+# print(f"x = {x}")
+x = int(input("Введите x = "))
+if x<0:
+    print("x - отрицательное число")
+elif x==0:
+    print("x = 0")
+else:
+    print("x - положительное число")
 
 
 
