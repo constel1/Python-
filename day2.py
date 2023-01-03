785b6f2c0c88ae2968c5028907152c8398939709
Andrey Makarov <makarov21a@yandex.ru>
2023-01-03 12:42:16 +0300

    new lesson - multidimensional array

--- day2.py
+++ day2.py
@@ -1,28 +1,36 @@
-a = [1,4,-45,7,98,-3,2]
+line = [1,7,6,11,3]
+img = [[1,7,6,11,3], [1,7,6,11,3], [1,7,6,11,3], [1,7,6,11,3], [1,7,6,11,3]]
+print(img)
+img = [line[:], line[:], line[:], line[:], line[:]]
+print(img)
+print(img[3])
+print(img[2][1])
+img[1] = [0,0,0,0,0]
+print(img)
+img[1] = [0]*5
+print(img)
+img[1][2:] = [1] *5
+print(img)
+img[2][:] = [4]*6
+print(img)
+img.append("hdfjhdcfj")
+print(img)
+del img[0]
+print(img)
+print(img[4])
+a = [[[True, False],[1,2,3]], ["матрица", "вектор"]]
+print(a[0])#true...
+print(a[0][0][1])
 
-print(a)
-a.append([1,2,3])
-print(a)
-a.insert(0, True)
-print(a)
-a.remove(True)
-a.remove(True)
-print(a)
-print(a.pop())
-print(a)
-a.pop(4)
-print(a)
-a.append(True)
-a.insert(2, 1)
-print(a)
-print(a.count(1))
-print(a.index(1,4))
-print("ihsfh" in a)
-print(a)
-a.reverse()
-print(a)
-a.sort(reverse=True)
-print(a)
+
+
+
+
+
+
+
+
+
 
 
 
