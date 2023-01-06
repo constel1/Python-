a540d0c53db012f2303c5b80be9dba29336c66b0
Andrey Makarov <makarov21a@yandex.ru>
2023-01-06 20:52:32 +0300

    словари

--- day2.py
+++ day2.py
@@ -1,26 +1,62 @@
- #
-#
-# b = [[1,2,3], [4,5,6], [7,8,9]]
-# print(b)
-# a = [ x
-#      for row in b
-#      for x in row
-#      ]
+# # d = {"house":"дом",
+# #      "car":"машина",
+# #      "tree":"дерево",
+# #      "road":"дорога",
+# #      "river":"река",}
+# # print(d)
+# # print(d["house"])
+# # print(d["car"])
+# # a = dict(one =1, two = 2)
+# # print(a)
+# lst = [[2,"плохо"],[3,"удовлетворительно"],[4,"хорошо"]]
+# a = dict(lst)
 # print(a)
-# M, N = 3,4
-# matrix = [[a for a in range(M)] for j in range(N)]
-# print(matrix)
-# A = [[1,2,3], [4,5,6], [7,8,9]]
-# print(A)
-# A = [[a**2 for a in row] for row in A]
-# print(A)
-# A = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
-# A = [[row[i] for row in A] for i in range(len(A[0]))]
-# print(A)
-g = [u **2 for u in [x+1 for x in range(5)]]
-print(g)
-
-
-
-
-
+# a[True] = "истина"
+# a[False] = "ложь"
+# a[3] = 34
+# print(a)
+# print(len(a))
+# del a[2]
+# print(a)
+# print("bjsadj" in a)
+# a[34] = 3
+# print(a)
+# lst = ["+6","+5","+4","+3","+2","+1"]
+# a = dict.fromkeys(lst, "код страны")
+# print(a)
+# # a.clear()
+# # print(a)
+# a2 = a.copy()
+# print(a2)
+# a2["password"] = "werty"
+# print(a2.get("+3"))
+# print(a2)
+# print(a2.get("pasword", "not find"))
+# print(a2.setdefault("password", "pass"))
+# print(a2)
+# print(a2.setdefault("word", "pass"))
+# print(a2)
+# a2.pop("+2")
+# a2.pop("+3")
+# a2.pop("+4")
+# a2.pop("+5")
+# a2.pop("+6")
+# q = a2.pop("+7", False)
+# print(f"ERROR - {q}")
+# print(a2)
+# a = dict(a2)
+# print(a)
+# print(f"a - {id(a)}\na2 - {id(a2)}")
+a = {'+1': 'код страны', 'password': 'werty', 'word': 'pass'}
+a[3] = "three"
+a["competion"] = "соревнование"
+b = {'one': 1, 'two': 2, 'three': 3}
+# a.popitem()
+# print(a)
+# a.popitem()
+c = {**a, **b}
+a.update(b)
+print(a.keys())
+print(a.values())
+print(a)
+print(c)
